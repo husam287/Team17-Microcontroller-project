d7ec5e5a143219d451f835f04784d66f11bbbea4
Hossam Sherif <husam287@hotmail.com>
2021-06-06 15:19:19 +0200

    bug fixed

--- main.c
+++ main.c
@@ -8,9 +8,9 @@
 #include <inttypes.h>
 #include <string.h>
 
-//#########################################
-//############### LED STUFF ###############
-//#########################################
+//####################################################
+//############### LED STUFF AND PORT F ###############
+//####################################################
 void PortF_Init(void){
 SYSCTL_RCGCGPIO_R |= 0x00000020; // activate Port F
 while((SYSCTL_PRGPIO_R&0x00000020) == 0){};
@@ -19,20 +19,7 @@ GPIO_PORTF_DIR_R = 0x0E; // PF4,PF0 in, PF3-1 out
 GPIO_PORTF_DEN_R = 0x1F; // digital I/O on PF4-0
 }
 
-void LED_Init(){
-	PortF_Init();
-}
 
-void PortF_Output(uint32_t data){ // write PF3-PF1 outputs
-GPIO_PORTF_DATA_R = data;
-}
-
-void LED_Out(uint8_t x){
-	if(x==1)
-	PortF_Output(0x02);
-	else
-		PortF_Output(0x00);
-}
 
 //#########################################
 //############### PORT B ###############
